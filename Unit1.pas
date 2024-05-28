96bda0c35652e3cd8ac68393dfbe86c1ec35bba7
CarlosJR3 <carlos.fer.junior@gmail.com>
2024-05-28 12:00:11 -0300

    site em html e delphi

---
+++ Unit1.pas
@@ -0,0 +1,36 @@
+unit Unit1;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+
+type
+  TForm1 = class(TForm)
+  private
+    { Private declarations }
+    procedure createDirectory;
+
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm1.createDirectory;
+var
+ Patch : String;
+begin
+Patch := ExtractFilePath(ParamStr(0));
+      if not DirectoryExists(Patch +'HTML') then
+      CreateDir(Patch +'HTML');
+
+end;
+
+end.
