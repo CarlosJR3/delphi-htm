c0040d58ae2351da8518757081db1a1636b011b6
CarlosJR3 <carlos.fer.junior@gmail.com>
2024-05-29 12:21:18 -0300

    criando os caminhos pro arquivo

--- Unit1.pas
+++ Unit1.pas
@@ -8,6 +8,7 @@ uses
 
 type
   TForm1 = class(TForm)
+    procedure FormCreate(Sender: TObject);
   private
     { Private declarations }
     procedure createDirectory;
@@ -31,6 +32,56 @@ Patch := ExtractFilePath(ParamStr(0));
       if not DirectoryExists(Patch +'HTML') then
       CreateDir(Patch +'HTML');
 
+      if not DirectoryExists(Patch +'HTML\assets') then
+      CreateDir(Patch +'HTML\assets');
+
+       if not DirectoryExists(Patch +'HTML\assets\animatecss') then
+      CreateDir(Patch +'HTML\assets\animatecss');
+
+       if not DirectoryExists(Patch +'HTML\assets\bootstrap') then
+      CreateDir(Patch +'HTML\assets\bootstrap');
+
+       if not DirectoryExists(Patch +'HTML\assets\dropdown') then
+      CreateDir(Patch +'HTML\assets\dropdown');
+
+       if not DirectoryExists(Patch +'HTML\assets\formoid') then
+      CreateDir(Patch +'HTML\assets\formoid');
+
+       if not DirectoryExists(Patch +'HTML\assets\gallery') then
+      CreateDir(Patch +'HTML\assets\gallery');
+
+       if not DirectoryExists(Patch +'HTML\assets\images') then
+      CreateDir(Patch +'HTML\assets\images');
+
+       if not DirectoryExists(Patch +'HTML\assets\imagesloaded') then
+      CreateDir(Patch +'HTML\assets\imagesloaded');
+
+       if not DirectoryExists(Patch +'HTML\assets\masonry') then
+      CreateDir(Patch +'HTML\assets\masonry');
+
+       if not DirectoryExists(Patch +'HTML\assets\mobirise') then
+      CreateDir(Patch +'HTML\assets\mobirise');
+
+       if not DirectoryExists(Patch +'HTML\assets\smoothscroll') then
+      CreateDir(Patch +'HTML\assets\smoothscroll');
+
+           if not DirectoryExists(Patch +'HTML\assets\socicon') then
+      CreateDir(Patch +'HTML\assets\socicon');
+
+       if not DirectoryExists(Patch +'HTML\assets\theme') then
+      CreateDir(Patch +'HTML\assets\theme');
+
+       if not DirectoryExists(Patch +'HTML\assets\web') then
+      CreateDir(Patch +'HTML\assets\web');
+
+       if not DirectoryExists(Patch +'HTML\assets\ytplayer') then
+      CreateDir(Patch +'HTML\assets\ytplayer');
+end;
+
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+           createDirectory;
 end;
 
 end.
